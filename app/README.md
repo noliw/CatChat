6e43563f894ecc335eb2bc80332fd5f94d851669
noliw <noliw2017@gmail.com>
2023-10-11 02:46:18 -0700

    drawer bar

---
+++ app/README.md
@@ -0,0 +1,7 @@
+NOTES:
+* The default app bar is added using a theme
+* The android:theme attribute in android manifest.xml specifies which theme to apply
+* You define a <Style> element in one or more resource files
+* You can replace the default app bar with a that includes the latest android features
+* A material toolbar is a tool that works well with material themes
+* A menu resource file 
--- app/src/main/java/com/example/catchat/MainActivity.kt
+++ app/src/main/java/com/example/catchat/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,13 +26,20 @@ class MainActivity : AppCompatActivity() {
         // getting the reference to the navigation controller from the navogation host
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        // getting a reference to the drawerlayout
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         // this builds a configuration linking the toolbar to the navigation graph
         val builder = AppBarConfiguration.Builder(navController.graph)
+        // the toolbar will display a drawer icon with which to open the drawer
+        builder.setOpenableLayout(drawer)
         val appBarConfiguration = builder.build()
         // applying the configuration to the toolbar
         toolbar.setupWithNavController(navController, appBarConfiguration)
+        val naView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(naView, navController)
 
         // implementing bottom navigation bar and linking the bar to the navigation controller
+
     }
 
     // Add any menu items to the to the toolbar(the hellp menu item)
