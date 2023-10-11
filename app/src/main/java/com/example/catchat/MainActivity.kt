7d74205d43be73e43ce328df06b3d926cf49b925
noliw <noliw2017@gmail.com>
2023-10-11 01:48:15 -0700

    top menu bar

--- app/src/main/java/com/example/catchat/MainActivity.kt
+++ app/src/main/java/com/example/catchat/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 
 
@@ -15,13 +18,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        // getting the reference to the navigation controller from the navogation host
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        // this builds a configuration linking the toolbar to the navigation graph
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        // applying the configuration to the toolbar
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // implementing bottom navigation bar and linking the bar to the navigation controller
     }
 
+    // Add any menu items to the to the toolbar(the hellp menu item)
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    // Navigate to a destination when an item is clicked
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return item.onNavDestinationSelected(navController)
